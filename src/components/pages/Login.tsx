a592faed1e510f2b5b3419718936100a5651b2fa
wadadones <jones3429@outlook.jp>
2021-11-02 23:59:10 +0900

    extract login button from login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,6 +2,7 @@ import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
 import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout";
 import { FC, memo } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton"
 
 export const Login: FC = memo(() => {
   return (
@@ -11,7 +12,7 @@ export const Login: FC = memo(() => {
         <Divider my={4}/>
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="user id" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>Login</Button>
+          <PrimaryButton>Login</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
