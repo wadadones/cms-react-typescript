94dfadbf198bf800d076d82ec2b3da341c27cceb
wadadones <jones3429@outlook.jp>
2021-11-03 09:22:33 +0900

    add toast message in login

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,10 +2,12 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router";
+import { useMessage } from "./useMessage"
 
 export const useAuth = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const { showMessage } = useMessage();
   const login = useCallback(
     (id: string) => {
       setLoading(true)
@@ -14,11 +16,12 @@ export const useAuth = () => {
         .then((res) => {
           if (res.data) {
             history.push("/home")
+            showMessage({ title: "Success!", status: "success" })
           } else {
-            alert('cannot find user.')
+            showMessage({ title: "Cannot find user.", status: "error" })
           }
         })
-        .catch(() => { alert('cannot login.') })
+        .catch(() => { showMessage({ title: "Cannot login.", status: "error" }) })
         .finally(() => { setLoading(false) })
     },
     [],
