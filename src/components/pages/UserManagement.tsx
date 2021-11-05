2841e7b1ac1c0afb1188b21933bf4c807f376662
wadadones <jones3429@outlook.jp>
2021-11-05 09:21:38 +0900

    show users

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,27 @@
-import { Wrap, WrapItem, Box, Stack, Image, Text } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { Wrap, WrapItem, Box, Stack, Image, Text, Spinner, Center } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
 import { UserCard } from "../organisms/user/UserCard"
+import { useAllUsers } from "../../hooks/useAllUsers"
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }} >
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/photos/random" userName="don" fullName="dondon"/>
-      </WrapItem>
-    </Wrap>
+    <>
+      { loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} >
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard imageUrl="https://source.unsplash.com/photos/random" userName={user.username} fullName={user.name}/>
+            </WrapItem>
+          ))}
+        </Wrap>
+      ) }
+    </>
   )
 });
